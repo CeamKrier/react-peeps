37844f74a1d7312bfca04cfdbce0bfd32b47dde1
Emre Çakır <ceamkrier@gmail.com>
2020-03-31 00:39:42 +0300

    docs: showcase updated with possible outputs

--- src/dev/showcase.tsx
+++ src/dev/showcase.tsx
@@ -30,52 +30,87 @@ const styles = {
 
 const Showcase: React.FC = () => {
 	return (
-		<div style={styles.showcaseWrapper}>
-			<Peep
-				style={{ ...styles.peepStyle }}
-				body='DocProtectiveClothe'
-				face='CalmNM'
-				hair='DocShield'
-				viewBox={{ x: '-20', y: '500', width: '1050', height: '2400' }}
-			/>
+		<>
+			<div style={styles.showcaseWrapper}>
+				<Peep
+					style={{ ...styles.peepStyle }}
+					body='DocProtectiveClothe'
+					face='CalmNM'
+					hair='DocShield'
+					viewBox={{ x: '-20', y: '500', width: '1050', height: '2400' }}
+				/>
 
-			<Peep
-				style={styles.peepStyle}
-				accessory='GlassRoundThick'
-				body='Shirt'
-				face='Cute'
-				hair='ShortVolumed'
-				facialHair='Dali'
-				strokeColor='black'
-				backgroundColor={{
-					degree: 310,
-					firstColor: '#FF0015',
-					secondColor: 'gold'
-				}}
-			/>
+				<Peep
+					style={styles.peepStyle}
+					accessory='GlassRoundThick'
+					body='Shirt'
+					face='Cute'
+					hair='ShortVolumed'
+					facialHair='Dali'
+					strokeColor='black'
+					backgroundColor={{
+						degree: 310,
+						firstColor: '#FF0015',
+						secondColor: 'gold'
+					}}
+				/>
 
-			<Peep
-				style={{ ...styles.peepStyle, transform: 'scale(-1, 1)' }}
-				accessory='GlassRoundThick'
-				body='Shirt'
-				face='Cute'
-				hair='ShortVolumed'
-				facialHair='Dali'
-				strokeColor={{
-					degree: 300,
-					firstColor: '#2E8A57',
-					secondColor: '#81087F'
-				}}
-			/>
+				<Peep
+					style={{ ...styles.peepStyle, transform: 'scale(-1, 1)' }}
+					accessory='GlassRoundThick'
+					body='Shirt'
+					face='Cute'
+					hair='ShortVolumed'
+					facialHair='Dali'
+					strokeColor={{
+						degree: 300,
+						firstColor: '#2E8A57',
+						secondColor: '#81087F'
+					}}
+				/>
 
-			<Peep
-				style={{ ...styles.peepStyle, transform: 'scale(-1, 1)' }}
-				body='DocStethoscope'
-				face='SmileNM'
-				hair='DocSurgery'
-				viewBox={{ x: '-20', y: '500', width: '1050', height: '2400' }}
-			/>
-		</div>
+				<Peep
+					style={{ ...styles.peepStyle, transform: 'scale(-1, 1)' }}
+					body='Doc'
+					face='CalmNM'
+					hair='DocSurgery'
+					viewBox={{ x: '-20', y: '500', width: '1050', height: '2400' }}
+				/>
+			</div>
+			<div style={styles.showcaseWrapper}>
+				<Peep
+					style={{ ...styles.peepStyle }}
+					body='WheelChair'
+					face='Calm'
+					hair='Long'
+					viewBox={{ x: '-20', y: '500', width: '1050', height: '2400' }}
+				/>
+
+				<Peep
+					style={{ ...styles.peepStyle }}
+					body='RoboDanceBW'
+					face='Cheeky'
+					hair='Short'
+					viewBox={{ x: '-20', y: '500', width: '1050', height: '2400' }}
+				/>
+
+				<Peep
+					style={{ ...styles.peepStyle, transform: 'scale(-1, 1)' }}
+					body='Bike'
+					face='Awe'
+					hair='BunFancy'
+					viewBox={{ x: '-20', y: '500', width: '1050', height: '2400' }}
+				/>
+
+				<Peep
+					style={{ ...styles.peepStyle, transform: 'scale(-1, 1)' }}
+					body='MediumBW'
+					face='Cute'
+					hair='MediumBangs'
+					viewBox={{ x: '-20', y: '500', width: '1050', height: '2400' }}
+				/>
+			</div>
+		</>
 	);
 };
 
